0c60203753e683af225026bbdb0a71375c1581e6
GGCristo <cgg.code@gmail.com>
2021-10-03 12:51:43 +0100

    Little improvements

--- pkg/ourImage/ourImage.go
+++ pkg/ourImage/ourImage.go
@@ -11,42 +11,38 @@ import (
 const weigh, heigh = 320, 200
 
 type ourImage struct {
-	// r [weigh][heigh]uint32
-	// g [weigh][heigh]uint32
-	// b [weigh][heigh]uint32
-	r    [][]uint32 // Is an slice because otherwhise encoding is too slow
+  // Is a slice because otherwhise encoding would be too slow, this way we avoid making heavy copies
+	r    [][]uint32
 	g    [][]uint32
 	b    [][]uint32
-	Rect image.Rectangle
+	rect image.Rectangle
 }
 
 func NewImage(path string) (ourImage, error) {
-	f, err := os.Open(path)
-	instance := ourImage{}
-	defer f.Close()
+	f, err := os.Open(path) // Open file
 	if err != nil {
 		return ourImage{}, err
 	}
-	rawImage := make([]byte, weigh*heigh)
+	defer f.Close()
+	rawImage := make([]byte, weigh*heigh) // Read raw image
 	_, err = f.Read(rawImage)
 	if err != nil {
 		return ourImage{}, err
 	}
-	instance.r = make([][]uint32, weigh)
-	for i := range instance.r {
-		instance.r[i] = make([]uint32, heigh)
-	}
-	instance.g = make([][]uint32, weigh)
-	for i := range instance.g {
-		instance.g[i] = make([]uint32, heigh)
-	}
-	instance.b = make([][]uint32, weigh)
-	for i := range instance.b {
-		instance.b[i] = make([]uint32, heigh)
-	}
+	instance := ourImage{}
+  initialize := func() [][]uint32 {
+    colorLayer := make([][]uint32, weigh)
+    for i := range colorLayer {
+      colorLayer[i] = make([]uint32, heigh)
+    }
+    return colorLayer
+  }
+  instance.r = initialize()
+  instance.g = initialize()
+  instance.b = initialize()
 	grayImage := image.NewGray(image.Rect(0, 0, weigh, heigh))
 	grayImage.Pix = rawImage
-	instance.Rect = grayImage.Bounds()
+	instance.rect = grayImage.Bounds()
 	for y := 0; y < grayImage.Bounds().Dy(); y++ {
 		for x := 0; x < grayImage.Bounds().Dx(); x++ {
 			instance.Set(x, y, grayImage.At(x, y))
@@ -56,19 +52,19 @@ func NewImage(path string) (ourImage, error) {
 }
 
 func (self *ourImage) Set(x, y int, c color.Color) {
-	if !(image.Point{x, y}.In(self.Rect)) {
+	if !(image.Point{x, y}.In(self.rect)) {
 		fmt.Printf("ourImage: Set x: %v, y: %v is outside bounds\n", x, y)
 		return
 	}
-	self.r[x][y], self.g[x][y], self.b[x][y], _ = c.RGBA()
-	self.r[x][y] >>= 8
-	self.g[x][y] >>= 8
-	self.b[x][y] >>= 8
+  r, g, b, _ := c.RGBA()
+	self.r[x][y] = r >> 8
+	self.g[x][y] = g >> 8
+	self.b[x][y] = b >> 8
 }
 
 // Help to implement image.Image
 func (self ourImage) At(x, y int) color.Color {
-	if !(image.Point{x, y}.In(self.Rect)) {
+	if !(image.Point{x, y}.In(self.rect)) {
 		fmt.Printf("ourImage: At x: %v, y: %v is outside bounds\n", x, y)
 		return color.Gray{}
 	}
@@ -82,7 +78,7 @@ func (self ourImage) ColorModel() color.Model {
 
 // Help to implement image.Image
 func (self ourImage) Bounds() image.Rectangle {
-	return self.Rect
+	return self.rect
 }
 
 func (self ourImage) Encode(path string) error {
