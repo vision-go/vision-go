1448f30f1cf033388795b3a1de8985301a70e82e
GGCristo <cgg.code@gmail.com>
2021-10-03 12:03:43 +0100

    Show image in a window

--- main.go
+++ main.go
@@ -7,16 +7,20 @@ import (
 	"strings"
 
 	// "github.com/pkg/profile"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	// "fyne.io/fyne/v2/theme"
 	"github.com/vision-go/vision-go/pkg/ourImage"
 )
 
 // const target = "bogart.tfe"
 // const target = "animal.tfe"
 // const target = "BUGS.tf"
-// const target = "caras.tfe"
+const target = "caras.tfe"
 // const target = "IMPLANTE.tfe"
 // const target = "MONTANIA.tfe"
-const target = "PLAYA2.tfe"
+// const target = "PLAYA2.tfe"
 
 func main() {
 	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
@@ -29,4 +33,11 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(image.At(40, 50).RGBA())
+	myApp := app.New()
+	w := myApp.NewWindow("Vision")
+  w.Resize(fyne.NewSize(float32(image.Bounds().Dx()), float32(image.Bounds().Dy())))
+  imageCanvas := canvas.NewImageFromImage(image)
+	imageCanvas.FillMode = canvas.ImageFillContain
+	w.SetContent(imageCanvas)
+	w.ShowAndRun()
 }
