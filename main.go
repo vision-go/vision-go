a1951d6a5d8d5c1a18d46ca322771c126366147f
GGCristo <cgg.code@gmail.com>
2021-10-17 13:01:21 +0100

    New ourImage structure

--- main.go
+++ main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-	// "fmt"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
+	"image/png"
 	// "github.com/pkg/profile"
 	"github.com/vision-go/vision-go/pkg/ourImage"
 )
 
 // const target = "bogart.tfe"
-const target = "animal.tfe"
+// const target = "animal.tfe"
 // const target = "BUGS.tf"
 // const target = "caras.tfe"
 // const target = "IMPLANTE.tfe"
 // const target = "MONTANIA.tfe"
 // const target = "PLAYA2.tfe"
+const target = "pumba.png"
 
 func main() {
 	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
-	image, err := ourimage.NewImage("images/" + target)
+	image, err := ourimage.NewImage(target)
 	if err != nil {
 		log.Fatal(err)
 	}
-  image = image.Negative()
-	err = image.Encode(strings.ReplaceAll(target, ".tfe", ".png"))
+  newImage := ourimage.Negative(image)
+  f, err := os.Create(strings.ReplaceAll(target, ".tfe", ".png"))
+  if err != nil {
+    log.Fatal(err)
+  }
+	err = png.Encode(f, newImage)
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/ourImage/ourImage.go
+++ pkg/ourImage/ourImage.go
@@ -3,122 +3,40 @@ package ourimage
 import (
 	"fmt"
 	"image"
-	"image/color"
-	"image/png"
+	_ "image/jpeg"
+	_ "image/png"
 	"os"
-  "sync"
+
+	"image/draw"
+  "image/color"
 )
 
-const weigh, heigh = 320, 200
-
-type ourImage struct {
-	// Is a slice because otherwhise encoding would be too slow, this way we avoid making heavy copies
-	r    [][]uint32
-	g    [][]uint32
-	b    [][]uint32
-	rect image.Rectangle
-}
-
-func NewImage(path string) (ourImage, error) {
-	f, err := os.Open(path) // Open file
+func NewImage(path string) (draw.Image, error) {
+	f, err := os.Open(path)
 	if err != nil {
-		return ourImage{}, err
+		return nil, err
 	}
 	defer f.Close()
-
-	instance := ourImage{} // Initialize slices
-	initialize := func() [][]uint32 {
-		colorLayer := make([][]uint32, weigh)
-		for i := range colorLayer {
-			colorLayer[i] = make([]uint32, heigh)
-		}
-		return colorLayer
-	}
-	instance.r = initialize()
-	instance.g = initialize()
-	instance.b = initialize()
-
-	grayImage := image.NewGray(image.Rect(0, 0, weigh, heigh))
-	rawImage := make([]byte, weigh*heigh) // Read raw image
-	_, err = f.Read(rawImage)
-	if err != nil {
-		return ourImage{}, err
-	}
-	grayImage.Pix = rawImage
-	instance.rect = grayImage.Bounds()
-	for y := 0; y < grayImage.Bounds().Dy(); y++ {
-		for x := 0; x < grayImage.Bounds().Dx(); x++ {
-			instance.Set(x, y, grayImage.At(x, y))
-		}
-	}
-	return instance, nil
+  img, format, err := image.Decode(f)
+  if err != nil {
+    return nil, err
+  }
+  dimg, ok := img.(draw.Image)
+  if !ok {
+    return nil, fmt.Errorf("%v, is not a valid format", format)
+  }
+  return dimg, nil
 }
 
-func (self *ourImage) Set(x, y int, c color.Color) {
-	if !(image.Point{x, y}.In(self.rect)) {
-		fmt.Printf("ourImage: Set x: %v, y: %v is outside bounds\n", x, y)
-		return
-	}
-	r, g, b, _ := c.RGBA()
-	self.r[x][y] = r >> 8
-	self.g[x][y] = g >> 8
-	self.b[x][y] = b >> 8
-}
-
-// Help to implement image.Image
-func (self ourImage) At(x, y int) color.Color {
-	if !(image.Point{x, y}.In(self.rect)) {
-		fmt.Printf("ourImage: At x: %v, y: %v is outside bounds\n", x, y)
-		return color.Gray{}
-	}
-	return color.Gray{Y: uint8(self.r[x][y])}
-}
-
-// Help to implement image.Image
-func (self ourImage) ColorModel() color.Model {
-	return color.GrayModel
-}
-
-// Help to implement image.Image
-func (self ourImage) Bounds() image.Rectangle {
-	return self.rect
-}
-
-func (self ourImage) Encode(path string) error {
-	outfile, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer outfile.Close()
-	png.Encode(outfile, self)
-	return nil
-}
-
-// Functions
-// TODO Better performance. Don't make a copy if you are going to overwrite it anyway
-func (self ourImage) Negative() ourImage {
-  negativeImage := self
-  negativeF := func(layer [][]uint32) {
-    for x := range layer {
-      for y := range layer[x] {
-        layer[x][y] = 255 - layer[x][y]
-      }
+func Negative(img draw.Image) draw.Image {
+  NewImage := img
+  for y := 0; y < img.Bounds().Dy(); y++ {
+    for x := 0; x < img.Bounds().Dx(); x++ {
+      col := img.At(x, y)
+      r, g, b, a := col.RGBA()
+      newCol := color.RGBA{uint8(255-r), uint8(255-g), uint8(255-b), uint8(a)}
+      NewImage.Set(x, y, newCol)
     }
   }
-  var wg sync.WaitGroup
-  wg.Add(3)
-  go func() {
-    negativeF(negativeImage.r)
-    wg.Done()
-  }()
-  go func() {
-    negativeF(negativeImage.g)
-    wg.Done()
-  }()
-  go func() {
-    negativeF(negativeImage.b)
-    wg.Done()
-  }()
-  wg.Wait()
-  return negativeImage
+  return NewImage
 }
